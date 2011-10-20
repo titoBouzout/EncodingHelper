ab73a0b37d7bcefdad15d95f533f639dc49c16d4
Tito Bouzout <tito.bouzout@gmail.com>
2011-10-20 04:58:59 -0300

    First public version

---
+++ EncodingHelper.py
@@ -0,0 +1,243 @@
+# coding=utf8
+import sublime, sublime_plugin
+import codecs
+import sys
+import os
+sys.path.append(os.path.join(sublime.packages_path(), 'EncodingHelper', 'chardet'))
+from chardet.universaldetector import UniversalDetector
+import re
+import functools
+import threading
+import time
+
+# don't parse binary files, just mark these as binary
+				 						#IMAGES----------------------#sublime---------------#fonts-----------#compressed
+BINARY = re.compile('(png|jpg|gif|jpeg|psd|ai|cdr|cache|sublime-package|eot|svgz|ttf|woff|zip|tar|gz|rar)$', re.I);
+
+SETTINGS = sublime.load_settings('EncodingHelper.sublime-settings')
+
+class EncodingOnStatusBarListener(sublime_plugin.EventListener):
+
+	def on_load(self, v):
+
+		# if enabled, show encoding on status bar 
+		if bool(SETTINGS.get('show_encoding_on_status_bar', True)):
+
+			# mark as loading
+			v.settings().set('encoding_helper_loading', True)
+
+			#has cached state?
+			if v.settings().has('encoding_helper_encoding'):
+				v.settings().erase('encoding_helper_loading')
+				v.set_status('encoding_helper_statusbar', v.settings().get('encoding_helper_encoding'))
+			else:
+				# is the file is there
+				file_name = v.file_name()
+				if not file_name or file_name == '' or os.path.isfile(file_name) == False:
+					v.settings().erase('encoding_helper_loading')
+					v.set_status('encoding_helper_statusbar', '');
+				#guess
+				else:
+					v.set_status('encoding_helper_statusbar', '');
+					#print 'GuessEncoding'
+					GuessEncoding(file_name, SETTINGS.get('fallback_encodings'), v).start()
+		else:
+			v.erase_status('encoding_helper_statusbar')
+	
+	def on_activated(self, v):
+		if bool(SETTINGS.get('show_encoding_on_status_bar', True)):
+			if v.settings().has('encoding_helper_loading'):
+				pass
+			else:
+				self.on_load(v)
+		else:
+			v.erase_status('encoding_helper_statusbar')
+
+SETTINGS.add_on_change('show_encoding_on_status_bar', lambda: EncodingOnStatusBarListener().on_load(sublime.active_window().active_view()))
+
+class GuessEncoding(threading.Thread):
+
+	def __init__(self, file_name, fallback_encodings = [], v = False,  callback = False):
+		threading.Thread.__init__(self)
+		self.file_name = file_name
+
+		self.fallback_encodings = fallback_encodings
+		self.v = v
+		if callback == False:
+			self.callback = self.on_done
+		else:
+			self.callback = callback
+
+	def run(self):
+		confidence = 0
+		if BINARY.search(self.file_name):
+			encoding = 'BINARY'
+			confidence = 1
+		elif maybe_binary(self.file_name):
+			encoding = 'BINARY'
+			confidence = 0.7
+		elif os.stat(self.file_name).st_size > 1048576: # skip files > 1Mb
+			encoding = 'Unknown'
+			confidence = 1
+		else:
+			started_at  = time.time()
+			timeout = False
+
+			detector = UniversalDetector()
+			fp = open(self.file_name, 'rb')
+			line = fp.readline(500)
+			while line != '':
+				detector.feed(line)
+				if time.time() - started_at > 8:
+					timeout = True
+					break
+				line = fp.readline(8000)
+			fp.close()
+			detector.close()
+			if timeout == False:
+				encoding = str(detector.result['encoding']).upper()
+				confidence = detector.result['confidence']
+			else:
+				encoding = 'Unknown'
+				confidence = 1
+			del detector
+
+			if encoding == None or encoding == 'NONE' or encoding == '':
+				encoding = 'BINARY'
+			elif encoding == 'ASCII':
+				encoding = 'UTF-8' 
+			elif confidence < 0.7:
+				fallback = self.test_fallback_encodings()
+				if fallback == False:
+					encoding = 'Unknown'
+				else:
+					encoding = fallback
+
+		sublime.set_timeout(functools.partial(self.callback, encoding), 0)
+
+	def test_fallback_encodings(self):
+		for encoding in self.fallback_encodings:
+			try:
+				fp = codecs.open(self.file_name, "rb", encoding.lower(), errors='strict')
+				line = fp.readline(500)
+				while line != '':
+					line = fp.readline(8000)
+				fp.close()
+				return encoding
+			except UnicodeDecodeError:
+				fp.close()
+		return False
+
+	def on_done(self, encoding):
+		if self.v != False:
+			self.v.settings().set('encoding_helper_encoding', encoding)
+			self.v.settings().erase('encoding_helper_loading')
+			self.v.set_status('encoding_helper_statusbar', encoding)
+			if encoding in SETTINGS.get('open_automatically_as_utf8', []):
+				ConvertToUTF8(self.file_name, encoding, self.v).start()
+
+class Toutf8fromBestGuessCommand(sublime_plugin.WindowCommand):
+	
+	def run(self):
+		encoding = sublime.active_window().active_view().settings().get('encoding_helper_encoding')
+		if encoding != None and encoding != 'UTF-8' and encoding != 'BINARY' and encoding != 'Unknown' and encoding != '':
+			Toutf8fromCommand(sublime_plugin.WindowCommand).run(encoding)
+
+	def description(self):
+		try:
+			encoding = sublime.active_window().active_view().settings().get('encoding_helper_encoding')
+			if encoding != None and encoding != 'UTF-8' and encoding != 'BINARY' and encoding != 'Unknown' and encoding != '':
+				return 'Convert to UTF-8 From '+encoding
+			else:
+				return 'Convert to UTF-8 From Best Guess'
+		except:
+			return 'Convert to UTF-8 From Best Guess'
+
+	def is_enabled(self):
+		try:
+			encoding = sublime.active_window().active_view().settings().get('encoding_helper_encoding')
+			if encoding != None and encoding != 'UTF-8' and encoding != 'BINARY' and encoding != 'Unknown' and encoding != '':
+				return True
+		except:
+			return False
+
+class Toutf8fromCommand(sublime_plugin.WindowCommand):
+	
+	def run(self, encoding = ''):
+		try:
+			if encoding == None or encoding == 'UTF-8' or encoding == 'BINARY' or encoding == 'Unknown' or encoding == '':
+				return False
+			v = sublime.active_window().active_view()
+			file_name = v.file_name()
+			if not file_name or file_name == '' or os.path.isfile(file_name) == False:
+				return False
+			else:
+				ConvertToUTF8(file_name, encoding, v).start()
+				return True
+		except:
+			return False
+
+	def is_enabled(self, encoding = ''):
+		try:
+			file_name = sublime.active_window().active_view().file_name()
+			if not file_name or file_name == '' or os.path.isfile(file_name) == False:
+				return False
+			else:
+				return True
+		except:
+			return False
+
+class ConvertToUTF8(threading.Thread):
+
+	def __init__(self, file_name, encoding, v = False,  callback = False):
+		threading.Thread.__init__(self)
+		self.file_name = file_name
+
+		self.encoding = encoding
+		self.v = v
+		if callback == False:
+			self.callback = self.on_done
+		else:
+			self.callback = callback
+
+	def run(self):
+		try:
+			content = codecs.open(self.file_name, "rb", self.encoding.lower(), errors='strict').read()
+			if len(content) != 0:
+				sublime.set_timeout(functools.partial(self.callback, content, self.encoding), 0)
+		except UnicodeDecodeError:
+			sublime.set_timeout(functools.partial(self.on_error, self.file_name, self.encoding), 0)
+		except LookupError:
+			sublime.set_timeout(functools.partial(self.on_lookup_error, self.file_name, self.encoding), 0)
+
+	def on_done(self, content, encoding):
+		if self.v != False:
+			edit = self.v.begin_edit()
+			self.v.replace(edit, sublime.Region(0, self.v.size()), content);
+			self.v.end_edit(edit)
+			self.v.settings().set('encoding_helper_encoding',  'UTF-8')
+			if bool(SETTINGS.get('show_encoding_on_status_bar', True)):
+				self.v.set_status('encoding_helper_statusbar', 'UTF-8')
+			self.v.set_status('encoding_helper_statusbar_converted_from', 'Converted to UTF-8 from '+encoding)
+	
+	def on_error(self, file_name, encoding):
+		sublime.error_message('Encoding helper message, unable to convert to UTF-8 the file \n'+file_name+' \nFrom encoding "'+encoding+'"');
+	
+	def on_lookup_error(self, file_name, encoding):
+		sublime.error_message('Encoding helper message, the encoding "'+encoding+'" is unknown in this system.\n Unable to convert to UTF-8 the file \n'+file_name+' \nFrom encoding "'+encoding+'"');
+
+def maybe_binary(file_name):
+	fp = open(file_name, 'rb')
+	line = fp.readline(500)
+	read = 500
+	while line != '':
+		if '\0' in line:
+			fp.close()
+			return True
+		read += 8000
+		if read > 1048576:
+			fp.close()
+			return False
+		line = fp.readline(8000)
+	fp.close()
+	return False	
---
+++ readme.md
@@ -0,0 +1,81 @@
+Description
+------------------
+
+Encoding Helper is a [Sublime Text](http://www.sublimetext.com/ ) plug-in which provides the following features:
+
+ * attempts to guess encoding of files
+ * show encoding on status bar
+ * convert to UTF-8 from a variete of encodings organized in a menu.
+ * convert to UTF-8 quickly from guessed encoding via menuitem
+ * convert to UTF-8 automatically when opening a file via some defined encodings found on User preferences
+
+You should know that attempt to guess the encoding of a file is hard and the results for some encodings is not 100% acurrated. Also, is a task that consume CPU, for this reason this plug-in includes a lot of optimizations.
+
+How it works
+------------------
+
+When "show_encoding_on_status_bar" is enabled and a file is opened, the plugin attempts to guess the encoding of the file, stopping on first result.
+
+* Check if the file name ends with some well know binary formats ( png, jpg, etc )
+ 	* Mark as binary
+* Check if the file contains the null character '\0' in the first 1048576 bytes (1Mb) of the file
+	* Mark as binary
+* If the file is larger than 1048576 bytes (1Mb) 
+	* Mark as Unknown
+* Give to the [chardet](http://pypi.python.org/pypi/chardet ) library no more than 8 seconds to guess the encoding of the file.
+	* if timed out (8 seconds) mark as Unknown
+	* if returned encoding "None" mark as Binary
+	* if returned encoding "ASCII" mark as "UTF-8"
+	* if the "confidence" result of the encoding is lower than 0.7, then try with the fallback_encodings set on User preferences. The first which is not failing will set as winner encoding, else set encoding as Unknown.
+
+For your sublime experience:
+
+* all of this runs on own thread.
+* files reads in chunks of 8000 bytes
+* results are cached in view.settings. Once a file encoding is guessed there is no need to guess the encoding again.
+
+Some more info
+------------------
+
+* I'm not an expert on encodings, don't talk to me as an expert. I just like a lot to build software compatible with every language using UTF-8
+* python chardet library is a port of chardet by Mozilla http://www-archive.mozilla.org/projects/intl/chardet.html
+* python chardet library is a 'Gone' project http://feedparser.org/
+* By looking for the null character '\0' on files, means that every UTF16 file will be marked as binary ( same as in git source )
+* When converting a file to UTF-8 from some other encoding, the plugin calls to codecs.open with the selected encoding as argument. See http://docs.python.org/library/codecs.html
+* If you are going to report that some file is reported with an incorrect encoding, please upload the file as is to some reliable resource and provide detailed information. Please be informed that even doing this I'm unable to look into the low levels on why this is happening. This is just to collect data that maybe is informative to someone curious on this topic.
+* Just for fun, It would be nice if someone can create a dataset of pure txt files with every char found in a complete list of character sets. This: http://www.mauvecloud.net/charsets/manual/index.html but with the files in this format: http://vancouver-webpages.com/multilingual/russian-koi.html ( without html, without imgs, just text )
+
+Resources
+------------------
+
+chardet library:
+http://pypi.python.org/pypi/chardet
+
+dataset small:
+http://vancouver-webpages.com/multilingual/
+
+dataset huge:
+http://www.mauvecloud.net/charsets/index.html
+
+codecs:
+http://docs.python.org/library/codecs.html
+
+Installation
+------------------
+
+ * Install this repository via "Package Control" http://wbond.net/sublime_packages/package_control
+
+Source-code
+------------------
+
+https://github.com/SublimeText/EncodingHelper
+
+Forum Thread
+------------------
+
+http://www.sublimetext.com/forum/viewtopic.php?f=x&t=xxxx
+
+Contribute
+------------------
+
+[Consider make a contribution](https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=extensiondevelopment%40gmail%2ecom&lc=UY&item_name=Tito&item_number=sublime%2dtext%2dside%2dbar%2dplugin&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted )
